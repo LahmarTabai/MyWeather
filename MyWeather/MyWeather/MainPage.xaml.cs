a53ad3cf7777bafd0ab494f118a2cf973383524a
LahmarTabai <Lahmartabai@yahoo.fr>
2023-05-11 23:46:53 +0200

    comment

--- MyWeather/MyWeather/MainPage.xaml.cs
+++ MyWeather/MyWeather/MainPage.xaml.cs
@@ -21,19 +21,18 @@ namespace MyWeather
             getMeteo();
         }
 
-        // c'est une méthode asynchrone : (complètement indépendante de l'application) à partir du moment ou on va appeler un serveur web; on sort de l'application donc il y'aura des temps d'attentes pour que la requette parte et le temps pour obtenir la réponse 
+        
         async void getMeteo()
         {
 
             try
             {
-                // création d'un client (Instensier Objet) : c'est ce qui nous permet de faire des requêtes vers un serveur (OpenWeatherMap)
-                using (HttpClient client = new HttpClient()) // using : elle détruit l'objet une fois qu'on a fini de l'utiliser
+                using (HttpClient client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/weather"); // le client web pointra sur l'adresse de base ici 
-                    string request = $"?q={cp.Text},fr&APPID=e2a9a1fbef788251baf0686f081f86a6&units=metric&lang=fr"; // c'est la requette qui s'effectuera par le client
-                    string jsonString = await client.GetStringAsync(request); // On va récuperer toute la requette dans une chaine de caractère "jsonString"
-                    var meteo = OpenWeatherMap.FromJson(jsonString); // 
+                    client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/weather"); 
+                    string request = $"?q={cp.Text},fr&APPID=e2a9a1fbef788251baf0686f081f86a6&units=metric&lang=fr";
+                    string jsonString = await client.GetStringAsync(request); 
+                    var meteo = OpenWeatherMap.FromJson(jsonString); 
                     temp.Text = meteo.Main.Temp.ToString() + " °C";
                     vent.Text = meteo.Wind.Speed.ToString() + " km/h";
                     hum.Text = meteo.Main.Humidity.ToString() + " %";
